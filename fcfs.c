50e03d76232b157d8d86852b889b69bce1c7b3f4
Aniket8195 <114986841+Aniket8195@users.noreply.github.com>
2023-11-22 14:23:43 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,62 +1,63 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
-struct pro {
- int id;
- int at;
- int bt;
- int ct;
- int tt;
- int wt;
+struct proc{
+    int id;
+    int at;
+    int bt;
+    int tt;
+    int wt;
+    int ct;
+    
 };
-void main(){
-   int n;
-   printf("Enter the number of processes");
-   scanf("%d",&n);
-   struct pro p[n];
-   for(int i=0;i<n;i++){
-     p[i].id=i+1;
-     printf("Enter Arrival time of process: %d ",i+1);
-     scanf("%d",&p[i].at);
-     printf("Enter Burst time of process: %d ",i+1);
-     scanf("%d",&p[i].bt);
-     p[i].ct=0;
-     p[i].tt=0;
-     p[i].wt=0;
-   }
 
-   struct pro temp;
-   for (int i = 0; i <n; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (p[j].at > p[j + 1].at) {
-                temp=p[j];
-		p[j]=p[j+1];
-		p[j+1]=temp;
-               
+void main(){
+    int n;
+    printf("Enter number of processes\n");
+    scanf("%d",&n);
+    struct proc P[n];
+    for(int i=0;i<n;i++){
+        printf("Enter the process %d details 1.Arrival Time ",i+1);
+        scanf("%d",&P[i].at);
+        printf("Burst time\n");
+        scanf("%d",&P[i].bt);
+        P[i].ct=0;
+        P[i].wt=0;
+        P[i].tt=0;
+    }
+    struct proc temp;
+    
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(P[i].at>P[j].at){
+                temp=P[i];
+                P[i]=P[j];
+                P[j]=temp;
             }
         }
-
     }
-   p[0].ct=p[0].bt;
-   int avgtt=0;
-   int avgwt=0;
-   for(int i=1;i<n;i++){
-     p[i].ct=p[i-1].ct + p[i].bt;
-   }
-   for(int i=0;i<n;i++){
-      p[i].tt=p[i].ct-p[i].at;
-      p[i].wt=p[i].tt-p[i].bt;
-      avgtt=avgtt+p[i].tt;
-      avgwt=avgwt+p[i].wt;
-   }
-
-   for(int i=0;i<n;i++){
-    printf("Process:%d  Arrival time:%d TT:%d WT:%d",p[i].id,p[i].at,p[i].tt,p[i].wt);
-    printf("\n");
-   }
-   printf("\n");
-   printf("Average TT is: %d ",(avgtt/n));
-   printf("Average WT is: %d ",(avgwt/n));
-   
+    
+    
+    P[0].ct=P[0].bt;
+    
+    for(int i=1;i<n;i++){
+        P[i].ct=P[i-1].ct+P[i].bt;
+ 
+    }
+    double avgtt=0,avgwt=0;
+	for(int i=0;i<n;i++){
+	    P[i].tt=P[i].ct-P[i].at;
+        P[i].wt=P[i].tt-P[i].bt;
+		avgtt+=P[i].tt;
+		avgwt+=P[i].wt;
+	}
+	printf("Average TT is %f and Average WT is %f\n",avgtt/n,avgwt/n);
+    for(int i=0;i<n;i++){
+        printf("P%d\t\t%d\t%d\n",P[i].id,P[i].at,P[i].bt);
+    }
+    
+    
+    
 }
+
+
+
 
